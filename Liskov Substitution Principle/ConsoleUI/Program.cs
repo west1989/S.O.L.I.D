0321ebb9a3174c791187fce8c5fcb5d2ba3f08c0
west1989 <gigiabakelia@gmail.com>
2020-06-30 12:30:28 +0400

    Add files via upload

---
+++ Liskov Substitution Principle/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IManager accountingVP = new CEO();
+
+      accountingVP.FirstName = "Emma";
+      accountingVP.LastName = "Stone";
+      accountingVP.CalculatePerHourRate(4);
+
+      Console.WriteLine($"{ accountingVP.FirstName }'s salary is ${ accountingVP.Salary }/hour.");
+
+      IManaged emp = new Manager();
+
+      emp.FirstName = "Tim";
+      emp.LastName = "Corey";
+      emp.AssignManager(accountingVP);
+      emp.CalculatePerHourRate(2);
+
+      Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+      
+      
+
+      Console.ReadLine();
+    }
+  }
+}
